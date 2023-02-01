22bce57c843e167458990083bea91a9f0c3b6c46
Het-Patel69 <phet9429@gmail.com>
2023-02-01 23:08:06 +0530

    Update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,24 +3,19 @@ import React from "react";
 function Home(props) {
     return (
         <div className={`min-h-screen ${props.mode}`}>
+
             <div className="container">
                 <div className="pt-4 mb-4">
                     <h4>About Me :</h4>
                     <p id="about_me">Hello! I'm Het Patel, I'm an intern at codding gujju and currently pursuing B.Tech from Silver Oak University, Ahmedabad. I'm a tech nerd so naturally I love to know and discuss about letest technologies used in smartphones and computers. I have a good knowledge of HTML, CSS and Java Script and I'm constatantly trying to improve and gain more knowledge.</p>
                 </div>
-                <div className="mt-4 pb-4">
+                <div className="my-4">
                     <h4>Personal Information :</h4>
                     <div id="inner_about">
                         <div>
                             Full Name :
                             <div>
                                 Het K. Patel
-                            </div>
-                        </div>
-                        <div>
-                            Birthdate :
-                            <div>
-                                09 January 2004
                             </div>
                         </div>
                         <div>
@@ -37,8 +32,7 @@ function Home(props) {
                         </div>
                         <div>
                             Address :
-                            <div>
-                                Vastral, Ahmedabad
+                            <div>Vastral, Ahmedabad
                             </div>
                         </div>
                     </div>
@@ -47,18 +41,13 @@ function Home(props) {
                     <h4>Projects :</h4>
                     <div id="projects">
                         <div>
+                            <a href="https://het-memegenerator.netlify.app" target='_blank'>
+                                Meme Generator
+                            </a>
+                        </div>
+                        <div>
                             <a href="https://het-sneakerstore.netlify.app" target='_blank'>
                                 Online Sneaker store
-                            </a>
-                        </div>
-                        <div>
-                            <a href="https://fyph7j.csb.app" target='_blank'>
-                                My Personal Portfolio using Bootstrap
-                            </a>
-                        </div>
-                        <div>
-                            <a href="https://het-patel.netlify.app" target='_blank'>
-                                My Personal Portfolio using React.js
                             </a>
                         </div>
                         <div>
@@ -70,6 +59,7 @@ function Home(props) {
                 </div>
             </div>
         </div>
+
     );
 }
 
